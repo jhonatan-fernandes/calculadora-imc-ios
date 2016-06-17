b5e1bc98d2322f6326b70407244d5f06bb95ff06
md10 <md10@SJ208-13.local>
2016-06-17 19:33:24 -0300

    fazendo segunda classe

---
+++ calculadoraImc/PessoaLastName.h
@@ -0,0 +1,13 @@
+//
+//  PessoaLastName.h
+//  calculadoraImc
+//
+//  Created by md10 on 6/17/16.
+//  Copyright © 2016 Abilio Bittencourt. All rights reserved.
+//
+
+#import "Pessoa.h"
+
+@interface PessoaLastName : Pessoa
+
+@end
---
+++ calculadoraImc/PessoaLastName.m
@@ -0,0 +1,13 @@
+//
+//  PessoaLastName.m
+//  calculadoraImc
+//
+//  Created by md10 on 6/17/16.
+//  Copyright © 2016 Abilio Bittencourt. All rights reserved.
+//
+
+#import "PessoaLastName.h"
+
+@implementation PessoaLastName
+
+@end
